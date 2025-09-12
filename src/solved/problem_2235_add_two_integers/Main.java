dea7ce0568008379b880ab7ae22266033361c979
Stefan Sandberg <nothxor@gmail.com>
2025-09-12 23:10:34 +0200

    Problem 2235, Add Two Integers, SOLVED.

---
+++ src/solved/problem_2235_add_two_integers/Main.java
@@ -0,0 +1,14 @@
+package solved.problem_2235_add_two_integers;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.sum(5,3));
+    }
+}
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
