8ae3afe80b2388659a0b574e41f5869e1678a802
Stefan Sandberg <nothxor@gmail.com>
2025-09-12 22:48:22 +0200

    Problem 9, Palindrome Number, SOLVED.

---
+++ src/solved/problem_9_palindrome_number/Main.java
@@ -0,0 +1,26 @@
+package solved.problem_9_palindrome_number;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(122));
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        char[] charArr = s.toCharArray();
+        int end = charArr.length - 1;
+        for (int start = 0; start < s.length(); start++) {
+            if (charArr[start] != charArr[end]) {
+                return false;
+            }
+            end--;
+        }
+        return true;
+    }
+}
