5772bd97730c5aece62735b663e35384468be84f
Stefan Sandberg <nothxor@gmail.com>
2025-09-12 23:08:31 +0200

    Fix problem 1672

--- src/solved/problem_1672_richest_customer_wealth/Main.java
+++ src/solved/problem_1672_richest_customer_wealth/Main.java
@@ -1,4 +1,26 @@
 package solved.problem_1672_richest_customer_wealth;
 
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] accounts = {{1,2,3},{3,2,1}};
+        System.out.println(solution.maximumWealth(accounts));
+    }
+}
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxSum = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int tempMax = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                tempMax += accounts[i][j];
+            }
+            if (tempMax > maxSum) {
+                maxSum = tempMax;
+            }
+        }
+        return maxSum;
+    }
 }
