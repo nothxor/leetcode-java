c5097730b6d6875a75ecd9836be580e4d0f0e735
Stefan Sandberg <nothxor@gmail.com>
2025-09-12 22:59:14 +0200

    Problem 876, Middle Of Linked List, SOLVED.

---
+++ src/solved/problem_876_middle_of_linked_list/Main.java
@@ -0,0 +1,30 @@
+package solved.problem_876_middle_of_linked_list;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
+    }
+}
+
