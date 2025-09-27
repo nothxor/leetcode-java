346f5a3a344e98fd0abd9bb6e9fb1a02620be23e
Stefan Sandberg <nothxor@gmail.com>
2025-09-27 08:37:36 +0200

    Problem 28, Find the Index of the First Occurrence in a String

---
+++ src/in_progress/problem_28_find_the_index_of_the_first_occurrence_in_a_string/Main.java
@@ -0,0 +1,13 @@
+package in_progress.problem_28_find_the_index_of_the_first_occurrence_in_a_string;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
